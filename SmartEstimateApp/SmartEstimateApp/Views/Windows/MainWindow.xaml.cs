e291177a353968a4134684e5e919585c5b78ca60
Gutnov.Nikita <fanelog49@gmail.com>
2025-05-10 15:57:18 +0300

    hotfix

--- SmartEstimateApp/SmartEstimateApp/Views/Windows/MainWindow.xaml.cs
+++ SmartEstimateApp/SmartEstimateApp/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using SmartEstimateApp.Manager;
 using SmartEstimateApp.Navigation;
 using SmartEstimateApp.ViewModels;
 using SmartEstimateApp.Views.Pages;
@@ -11,22 +12,20 @@ namespace SmartEstimateApp.Views.Windows
     public partial class MainWindow : Window
     {
         private readonly IServiceProvider _serviceProvider;
-
         public Frame MainFrame { get; private set; }
-
         private MainWindowViewModel _viewModel;
+        private WindowResizeManager _resizeManager;
 
         public MainWindow(IServiceProvider serviceProvider)
         {
             InitializeComponent();
-
             _serviceProvider = serviceProvider;
-
             MainFrame = FindName("MainFrameU") as Frame;
-
-            _viewModel = _serviceProvider.GetService<MainWindowViewModel>();
-
+            _viewModel = serviceProvider.GetService<MainWindowViewModel>();
             DataContext = _viewModel;
+
+            // Создаем экземпляр менеджера изменения размеров для текущего окна
+            _resizeManager = new WindowResizeManager(this);
 
             Loaded += MainWindow_Loaded;
         }
@@ -34,43 +33,30 @@ namespace SmartEstimateApp.Views.Windows
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             var navigationService = _serviceProvider.GetService<INavigationService>();
-
             navigationService.NavigateTo<LoginPage>();
+
+            var screen = System.Windows.SystemParameters.WorkArea;
+            MaxHeight = screen.Height;
         }
-        // Add these methods to your MainWindow.xaml.cs file
 
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ClickCount == 2)
-            {
-                MaximizeButton_Click(sender, e);
-            }
-            else
-            {
-                this.DragMove();
-            }
+            _resizeManager.HandleMouseLeftButtonDown(sender, e);
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            _resizeManager.Close();
         }
 
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
-            this.WindowState = WindowState.Minimized;
+            _resizeManager.Minimize();
         }
 
         private void MaximizeButton_Click(object sender, RoutedEventArgs e)
         {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.WindowState = WindowState.Normal;
-            }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-            }
+            _resizeManager.ToggleMaximize();
         }
     }
 }
