edf413e8ad5b68660be845af1b81c301e372e499
IgmoRqq <vagazz405@gmail.com>
2025-05-18 15:52:26 +0300

    feature: добавил изменение клиента

--- SmartEstimateApp/SmartEstimateApp/App.xaml.cs
+++ SmartEstimateApp/SmartEstimateApp/App.xaml.cs
@@ -9,6 +9,162 @@ namespace SmartEstimateApp
     /// </summary>
     public partial class App : Application
     {
+<<<<<<< Updated upstream
+=======
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var configuration = BuildConfiguration();
+            ServiceProvider = ConfigureServices(configuration);
+
+            AppDomain.CurrentDomain.UnhandledException += (s, exArgs) =>
+            {
+                var logger = ServiceProvider.GetService<ILogger<App>>();
+                if (logger != null)
+                {
+                    logger.LogCritical(exArgs.ExceptionObject as Exception, "Fatal error (UnhandledException)");
+                }
+            };
+
+            var userBL = ServiceProvider.GetService<IUserBL>();
+            var currentUser = ServiceProvider.GetService<CurrentUser>();
+            var credentialsManager = ServiceProvider.GetService<CredentialsManager>();
+
+            if (await TryAutoLoginAsync(userBL, currentUser, credentialsManager))
+            {
+                var homeWindow = ServiceProvider.GetService<HomeWindow>();
+                homeWindow.Show();
+            }
+            else
+            {
+                var mainWindow = ServiceProvider.GetService<MainWindow>();
+                mainWindow?.Show();
+            }
+        }
+
+        private IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+        }
+
+        private IServiceProvider ConfigureServices(IConfiguration configuration)
+        {
+            string connectionString = ConnectionStringManager.GetConnectionString(configuration);
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.MSSqlServer(
+                    connectionString: connectionString,
+                    sinkOptions: new MSSqlServerSinkOptions
+                    {
+                        TableName = "Logs",
+                        AutoCreateSqlTable = true
+                    },
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+                )
+                .CreateLogger();
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IConfiguration>(configuration);
+
+            services.AddSingleton<Serilog.ILogger>(Log.Logger);
+
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.AddSerilog();
+            });
+
+            services.AddAutoMapper(typeof(Profiles.MappingProfile));
+
+            services.AddBusinessLogic(options =>
+            {
+                options.EnableExtendedValidation = true;
+                options.MinPasswordLength = 10;
+                options.RequireComplexPassword = true;
+                options.MaxFailedLoginAttempts = 3;
+            });
+
+            services.AddDataAccess(configuration);
+
+            // Регистрация окон
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<HomeWindow>();
+
+            //Регистрация Managers зависимостей
+            services.AddSingleton<CurrentUser>(provider => CurrentUser.Instance);
+            services.AddSingleton<CredentialsManager>();
+
+            // Регистрация навигационных сервисов
+            services.AddSingleton<INavigationServiceFactory, NavigationServiceFactory>();
+            services.AddScoped<INavigationService, NavigationService>();
+
+            //Регистрация ViewModels
+            services.AddTransient<ClientsViewModel>();
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddSingleton<HomeWindowViewModel>();
+
+            // Регистрация страниц
+            services.AddScoped<LoginPage>();
+            services.AddScoped<RegisterPage>();
+            services.AddScoped<VerificationPage>();
+            services.AddScoped<PasswordResetPage>();
+            services.AddScoped<ResetEmailPage>();
+            services.AddTransient<DashboardPage>();
+            services.AddTransient<ProjectsPage>();
+            services.AddTransient<ClientsPage>();
+            services.AddScoped<SettingsPage>();
+            services.AddTransient<AnalyticsPage>();
+            services.AddTransient<ClientsEditPage>();
+            services.AddTransient<ClientEditViewModel>();
+
+
+            //Контекст зависимостей
+            services.AddScoped<ILoginContext, LoginContext>();
+            services.AddScoped<IRegisterContext, RegisterContext>();
+
+            return services.BuildServiceProvider();
+        }
+
+        private async Task<bool> TryAutoLoginAsync(IUserBL userBL, CurrentUser currentUser, CredentialsManager credentialsManager)
+        {
+            try
+            {
+                var (email, password, isValid) = credentialsManager.LoadCredentials();
+                if (!isValid || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+                {
+                    Console.WriteLine("Invalid or missing credentials");
+                    return false;
+                }
+
+                var user = await userBL.VerifyPasswordAsync(email, password);
+                if (user == null)
+                {
+                    Console.WriteLine("User verification failed");
+                    credentialsManager.ClearCredentials();
+                    return false;
+                }
+
+                var modelUser = Mapper.ToModel(user);
+                currentUser.SetUser(modelUser);
+                return true;
+            }
+            catch
+            {
+                credentialsManager.ClearCredentials();
+                return false;
+            }
+        }
+>>>>>>> Stashed changes
     }
 
 }
---
+++ SmartEstimateApp/SmartEstimateApp/Views/Pages/ClientsEditPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Bl.Interfaces;
+using SmartEstimateApp.Models;
+using SmartEstimateApp.ViewModels;
+using SmartEstimateApp.Navigation.Interfaces;
+using System.Windows.Controls;
+using Entities;
+
+namespace SmartEstimateApp.Views.Pages
+{
+    public partial class ClientsEditPage : Page
+    {
+        private readonly ClientEditViewModel _viewModel;
+
+        public ClientsEditPage(IClientBL clientBL, INavigationService navigationService, CurrentUser currentUser)
+        {
+            InitializeComponent();
+            _viewModel = new ClientEditViewModel(clientBL, navigationService, currentUser);
+            this.DataContext = _viewModel;
+        }
+
+        public void SetClient(Client client)
+        {
+            _viewModel.LoadClient(client);
+        }
+    }
+}
